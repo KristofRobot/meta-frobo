3d52ba72f323e2694a91bb56d2d95924eeafa9e2
Kristof Robot <krirobo@gmail.com>
2014-01-25 15:54:09 +0100

    initscripts: renamed cpufreq to perftune and added noop scheduling

--- recipes-core/initscripts/initscripts/perftune.sh
+++ recipes-core/initscripts/initscripts/perftune.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          cpufreq
+# Provides:         perftune 
 # Required-Start:
 # Required-Stop:
 # Default-Start:     S
 # Default-Stop:
-# Short-Description: Set CPU frequency profile
+# Short-Description: Tune performance 
 ### END INIT INFO
 echo -n "Setting ondemand CPU frequency profile"
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -15,3 +15,7 @@ echo 912000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 #echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 40 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
 echo 200000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+
+
+#set noop scheduler
+echo noop >  /sys/block/mmcblk0/queue/scheduler
--- recipes-core/initscripts/initscripts_1.0.bbappend
+++ recipes-core/initscripts/initscripts_1.0.bbappend
@@ -2,9 +2,9 @@ FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 # Don't forget to bump PRINC if you update the extra files.
 PRINC = "1"
 
-SRC_URI += "file://cpufreq.sh "
+SRC_URI += "file://perftune.sh "
 
 do_install_append () {
-	install -m 0755    ${WORKDIR}/cpufreq.sh	${D}${sysconfdir}/init.d
-	update-rc.d -r ${D} cpufreq.sh start 25 5 .
+	install -m 0755    ${WORKDIR}/perftune.sh	${D}${sysconfdir}/init.d
+	update-rc.d -r ${D} perftune.sh start 25 5 .
 }
